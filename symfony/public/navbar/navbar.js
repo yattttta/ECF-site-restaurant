eda4d1148e992736df62197f96e43b80e84dfa7e
yaTTTa <patate_o_gruyere@hotmail.fr>
2023-03-08 03:38:36 +0100

    #5 Intégration datepicker avec jquery-ui

--- symfony/public/navbar/navbar.js
+++ symfony/public/navbar/navbar.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    
     let toggle = document.querySelector('.toggle')
     let body = document.querySelector('body')
 
---
+++ symfony/public/pageReserver/pageReserver.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+
+    $(function() {
+        $( "#date1" ).datepicker({
+            closeText: 'Fermer',
+            prevText: 'Précédent',
+            nextText: 'Suivant',
+            changeMonth: true,
+            changeYear: true,
+            monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+            monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+            dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+            dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+            dateFormat: 'dd-mm-yy',
+            firstDay: 1,
+        });
+    });
+
+      
+})
