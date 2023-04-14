f8305776a019747df607d40aeb0e843f4f2ad265
yaTTTa <patate_o_gruyere@hotmail.fr>
2023-04-14 16:49:22 +0200

    #18 Responsive v1.4 - Page inscription

--- symfony/src/Form/UserType.php
+++ symfony/src/Form/UserType.php
@@ -6,22 +6,20 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, ['label' => 'Nom'])
-            ->add('prenom', TextType::class, ['label' => 'Prénom'])
-            ->add('email', EmailType::class, ['label' => 'Votre Email'])
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-            ->add('convives', IntegerType::class, ['label' => 'Nombre de convives', 'attr' => ['min' => 1, 'max' => 20]])
-            ->add('allergies', TextType::class, ['label' => 'Vos allergies'])
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('password')
+            ->add('convives')
+            ->add('allergies', TextType::class, ['required' => false])
         ;
     }
 
